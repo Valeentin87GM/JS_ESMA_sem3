279eff89d78992d5b96bcd1a20748c0cc1df9568
Valeentin87GM <valeentin87@list.ru>
2024-07-15 13:00:59 +0300

    Создал репозиторий и начал решать задание № 1

---
+++ task1.js
@@ -0,0 +1,45 @@
+/*Задание 1: ""Управление персоналом компании""
+
+Реализуйте класс Employee (сотрудник), который имеет следующие свойства и методы:
+
+Свойство name (имя) - строка, имя сотрудника.
+Метод displayInfo() - выводит информацию о сотруднике (имя).
+Реализуйте класс Manager (менеджер), который наследует класс Employee и имеет дополнительное свойство и метод:
+
+Свойство department (отдел) - строка, отдел, в котором работает менеджер.
+Метод displayInfo() - переопределяет метод displayInfo() родительского класса и выводит информацию о менеджере (имя и отдел).
+// Пример использования классов
+const employee = new Employee(""John Smith"");
+employee.displayInfo();
+// Вывод:
+// Name: John Smith
+
+const manager = new Manager(""Jane Doe"", ""Sales"");
+manager.displayInfo();
+// Вывод:
+// Name: Jane Doe
+// Department: Sales
+*/
+
+
+/**
+ * Класс Empoyee
+@param {string} name - имя сотрудника
+@param displayInfo - метод, который выводит информацию о сотруднике
+
+ */
+class Employee {
+    name;
+
+    constructor(name) {
+        this.name = name
+    }
+
+    displayInfo() {
+        console.log(`Сотрудника зовут ${this.name}`);
+    }
+
+}
+
+const employee1 = new Employee("Иван")
+
